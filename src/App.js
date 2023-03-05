cbb250e5a86002326e6404594aac1f0b73f473d8
Apurva <apurvapingale05@gmail.com>
2023-03-05 16:58:18 +0530

    adding home page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import './App.css';
-import Landing from "./pages/Landing"
-import Home from "./pages/Home"
+import "./App.css";
+import Landing from "./pages/Landing";
+import Home from "./pages/Home";
 import Login from "./pages/Login";
-import Signup from "./pages/Signup"
+import Signup from "./pages/Signup";
 function App() {
   return (
     <>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Landing/>}/>
-        <Route path="/home" element={<Home/>}/>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/signup" element={<Signup/>}/>
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          {/* <Route path="/" element={<Landing/>}/> */}
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
